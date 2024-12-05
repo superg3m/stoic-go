09742be5404c7ebac339755a83a4a17d86f33609
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-12-05 08:03:09 -0500

    Create add_user.go

---
+++ scripts/add_user.go
@@ -0,0 +1,17 @@
+package main
+
+// command line
+/*
+User's Email Address: superg3m@gmail.com
+Username: super
+User's Password: Testings123!
+Make Admin (Y/n) [N]: y
+
+Creating user account.. DONE
+Creating user profile.. DONE
+Assigning administrator role.. DONE
+*/
+
+func main() {
+
+}
